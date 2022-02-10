8111102539f75a0da0d5b5f1ff1bc6c3d43e4315
Stanly Sukmajaya Kwok <stanlyskwok@gmail.com>
2022-02-10 18:57:00 +0700

    tinggal connect metamask + mint

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,32 @@
 import '../styles/globals.css';
+import styles from '../styles/Home.module.css';
+import Image from 'next/image';
+import { Link } from 'react-scroll';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <nav className={styles.navbar}>
+        <div className={styles.logoContainer}>
+          <Image src='/images/bored-ape.png' width={70} height={70} />
+          <span className={styles.logoName}>escAPE</span>
+        </div>
+        <div>
+          <Link to="home" smooth={true} className={styles.navlink}>
+            Home
+          </Link>
+          <Link to='about' smooth={true} className={styles.navlink}>
+            About
+          </Link>
+          <Link to="roadmap" smooth={true} className={styles.navlink}>
+            Roadmap
+          </Link>
+          <Link to="contact" smooth={true} className={styles.navlink}>
+            Contact
+          </Link>
+        </div>
+        <a className={styles.connectWalletBtn}>CONNECT WALLET</a>
+      </nav>
       <video className="vid" src='/videos/video-1.mp4' autoPlay loop muted />
       <Component {...pageProps} />
     </div>
